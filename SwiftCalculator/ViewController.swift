f1e0d4d2f94290cdf6ccb09bfc3deb31113d7183
Ray P <rtepowers@gmail.com>
2015-06-23 22:35:53 -0700

    Initial Commit

--- SwiftCalculator/ViewController.swift
+++ SwiftCalculator/ViewController.swift
@@ -10,16 +10,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+	
+	@IBOutlet weak var display: UILabel!
+	
+	var isCurrentlyEntering: Bool = false
+
+	@IBAction func appendDigit(sender: UIButton) {
+		let digit = sender.currentTitle!
+		if (isCurrentlyEntering) {
+			display.text = display.text! + digit
+		} else {
+			display.text = digit
+			isCurrentlyEntering = true
+		}
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	
+	var operandStack = Array<Double>()
+	
+	@IBAction func enter() {
+		isCurrentlyEntering = false
+		operandStack.append(displayValue)
+		println("operandStack = \(operandStack)")
 	}
-
-
+	
+	var displayValue: Double {
+		get {
+			return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+		}
+		set {
+			display.text = "\(newValue)"
+			isCurrentlyEntering = false
+		}
+	}
+	
+	private func performOperation(operation: Double -> Double) {
+		if operandStack.count >= 1 {
+			displayValue = operation(operandStack.removeLast())
+			enter()
+		}
+	}
+	
+	func performOperation(operation: (Double, Double) -> Double) {
+		if operandStack.count >= 2 {
+			displayValue = operation(operandStack.removeLast(),operandStack.removeLast())
+			enter()
+		}
+	}
+	
+	@IBAction func operation(sender: UIButton) {
+		let operation = sender.currentTitle!
+		if (isCurrentlyEntering) {
+			enter()
+		}
+		switch operation {
+		case "+": performOperation { $0 + $1 }
+		case "-": performOperation { $1 - $0 }
+		case "÷": performOperation { $1 / $0 }
+		case "✕": performOperation { $0 * $1 }
+		default: break
+		}
+	}
+	
 }
 
